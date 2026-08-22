541a0526bd0efbfa8aebc2dfd619a4e5ef58888c
Henry Hu <henry.hu.sh@gmail.com>
2026-08-22 00:15:43 -0400

    Add ergogen configs for jegan

---
+++ footprints/choc_stabilizer.js
@@ -0,0 +1,43 @@
+module.exports = {
+    nets: {
+        net: undefined
+    },
+    params: {
+        class: 'HOLE',
+    },
+    body: p => `
+    (module Choc-2.25u-stabilizer (layer F.Cu) (attr smd)
+	${p.at}
+    (property "Reference" "${p.ref}"
+      (at 0 -15 ${p.r})
+      (layer "F.SilkS")
+      ${p.ref_hide}
+      (effects (font (size 1 1) (thickness 0.15)))
+    )
+    (fp_line (start -20.25 -8.5) (end 20.25 -8.5) (stroke (width 0.1524) (type solid) ) (layer "Dwgs.User") )
+    (fp_line (start -20.25 8.5) (end -20.25 -8.5) (stroke (width 0.1524) (type solid) ) (layer "Dwgs.User") )
+    (fp_line (start 20.25 -8.5) (end 20.25 8.5) (stroke (width 0.1524) (type solid) ) (layer "Dwgs.User") )
+    (fp_line (start 20.25 8.5) (end -20.25 8.5) (stroke (width 0.1524) (type solid) ) (layer "Dwgs.User") )
+    (fp_line (start -15.15 -3.8) (end -13.8 -3.8) (stroke (width 0.12) (type solid) ) (layer "Dwgs.User") )
+    (fp_line (start -15.15 3.05) (end -15.15 -3.8) (stroke (width 0.12) (type solid) ) (layer "Dwgs.User") )
+    (fp_line (start -15.15 3.05) (end -8.85 3.05) (stroke (width 0.12) (type solid) ) (layer "Dwgs.User") )
+    (fp_line (start -13.8 -8.45) (end -13.8 -3.8) (stroke (width 0.12) (type solid) ) (layer "Dwgs.User") )
+    (fp_line (start -13.8 -8.45) (end -10.2 -8.45) (stroke (width 0.12) (type solid) ) (layer "Dwgs.User") )
+    (fp_line (start -10.2 -8.45) (end -10.2 -3.8) (stroke (width 0.12) (type solid) ) (layer "Dwgs.User") )
+    (fp_line (start -10.2 -3.8) (end -8.85 -3.8) (stroke (width 0.12) (type solid) ) (layer "Dwgs.User") )
+    (fp_line (start -8.85 3.05) (end -8.85 -3.8) (stroke (width 0.12) (type solid) ) (layer "Dwgs.User") )
+    (fp_line (start 8.85 -3.8) (end 10.2 -3.8) (stroke (width 0.12) (type solid) ) (layer "Dwgs.User") )
+    (fp_line (start 8.85 3.05) (end 8.85 -3.8) (stroke (width 0.12) (type solid) ) (layer "Dwgs.User") )
+    (fp_line (start 8.85 3.05) (end 15.15 3.05) (stroke (width 0.12) (type solid) ) (layer "Dwgs.User") )
+    (fp_line (start 10.2 -8.45) (end 10.2 -3.8) (stroke (width 0.12) (type solid) ) (layer "Dwgs.User") )
+    (fp_line (start 10.2 -8.45) (end 13.8 -8.45) (stroke (width 0.12) (type solid) ) (layer "Dwgs.User") )
+    (fp_line (start 13.8 -8.45) (end 13.8 -3.8) (stroke (width 0.12) (type solid) ) (layer "Dwgs.User") )
+    (fp_line (start 13.8 -3.8) (end 15.15 -3.8) (stroke (width 0.12) (type solid) ) (layer "Dwgs.User") )
+    (fp_line (start 15.15 3.05) (end 15.15 -3.8) (stroke (width 0.12) (type solid) ) (layer "Dwgs.User") )
+
+    (fp_rect (start -14.65 -3.2) (end -9.35 2.3) (layer "Edge.Cuts") (stroke (width 0.12) (type solid) ) (fill none) )
+    (fp_rect (start -14 -9)	(end -10 -5.5)	(layer "Edge.Cuts") (stroke (width 0.12) (type solid) ) (fill none) )
+    (fp_rect (start 9.35 -3.2) (end 14.65 2.3) (stroke (width 0.12) (type solid) ) (layer "Edge.Cuts") (fill none) )
+    (fp_rect (start 10 -9) (end 14 -5.5) (stroke (width 0.12) (type solid) ) (layer "Edge.Cuts") (fill none) )
+    )`
+}
